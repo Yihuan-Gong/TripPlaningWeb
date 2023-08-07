b48323a4e7ae6588f55a8abc5294f88b26a8cb58
unknown <yihuan981@gmail.com>
2023-08-07 23:46:50 +0800

    New function: add A->B->C route with different colors

--- app/app.js
+++ app/app.js
@@ -25,38 +25,47 @@ function addNewPlan() {
 // }
 
 function initMap() {
-  const directionsRenderer = new google.maps.DirectionsRenderer();
-  const directionsService = new google.maps.DirectionsService();
+  // 初始化地圖
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 7,
     center: { lat: 25.046, lng: 121.517 },
     disableDefaultUI: true,
   });
 
-  // 顯示路徑在地圖上
-  directionsRenderer.setMap(map);
+  // 創建方向服務物件和路徑顯示圖層
+  const directionsRenderer = new google.maps.DirectionsRenderer();
+  const directionsService = new google.maps.DirectionsService();
+  directionsRenderer.setMap(map); // 將路徑畫在地圖上
+  // directionsRenderer.setPanel(document.getElementById("sidebar"));    // 逐行顯示經過道路
 
-  // 逐行顯示經過道路
-  // directionsRenderer.setPanel(document.getElementById("sidebar"));
+  // 計算路徑
+  let pos1 = { lat: 25.046, lng: 121.517 }; // 台北車站
+  let pos2 = { lat: 25.033, lng: 121.564 }; // 台北101
+  let pos3 = "捷運象山站";
+  calculateAndDisplayRoute(pos1, pos2, "#FF0000");
+  calculateAndDisplayRoute(pos2, pos3, "#0000FF");
 
-  calculateAndDisplayRoute(directionsService, directionsRenderer);
-}
-
-function calculateAndDisplayRoute(directionsService, directionsRenderer) {
-  const start = { lat: 25.046, lng: 121.517 }; // 台北車站
-  const end = { lat: 25.033, lng: 121.564 }; // 台北101
-  const selectedMode = "DRIVING";
-
-  directionsService
-    .route({
-      origin: start,
-      destination: end,
-      travelMode: google.maps.TravelMode[selectedMode],
-    })
-    .then((response) => {
-      directionsRenderer.setDirections(response);
-    })
-    .catch((e) => window.alert("Directions request failed"));
+  function calculateAndDisplayRoute(start, end, color) {
+    directionsService
+      .route({
+        origin: start,
+        destination: end,
+        travelMode: google.maps.TravelMode["DRIVING"],
+      })
+      .then((response) => {
+        directionsRenderer.setDirections(response);
+        let route = response.routes[0].overview_path;
+        let polyline = new google.maps.Polyline({
+          path: route,
+          geodesic: true,
+          strokeColor: color,
+          strokeOpacity: 0.8,
+          strokeWeight: 4,
+        });
+        polyline.setMap(map);
+      })
+      .catch((e) => window.alert("Directions request failed"));
+  }
 }
 
 addNewPlan();
