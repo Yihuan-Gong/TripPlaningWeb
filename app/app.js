3b0e7ebfc9f00c75f0d206cce167d0becd4e48fd
unknown <yihuan981@gmail.com>
2023-08-07 16:21:25 +0800

    Add google map route planning from A to B (hardcoding)

--- app/app.js
+++ app/app.js
@@ -14,6 +14,49 @@ function addNewPlan() {
   });
 }
 
-addNewPlan();
+// Initialize and add the map
+// function initMap() {
+//   let uluru = { lat: -25.344, lng: 131.036 };
+//   let map = new google.maps.Map(document.getElementById("map"), {
+//     zoom: 4,
+//     center: uluru,
+//   });
+//   let marker = new google.maps.Marker({ position: uluru, map: map });
+// }
 
-// This is a test line
+function initMap() {
+  const directionsRenderer = new google.maps.DirectionsRenderer();
+  const directionsService = new google.maps.DirectionsService();
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 7,
+    center: { lat: 25.046, lng: 121.517 },
+    disableDefaultUI: true,
+  });
+
+  // 顯示路徑在地圖上
+  directionsRenderer.setMap(map);
+
+  // 逐行顯示經過道路
+  // directionsRenderer.setPanel(document.getElementById("sidebar"));
+
+  calculateAndDisplayRoute(directionsService, directionsRenderer);
+}
+
+function calculateAndDisplayRoute(directionsService, directionsRenderer) {
+  const start = { lat: 25.046, lng: 121.517 }; // 台北車站
+  const end = { lat: 25.033, lng: 121.564 }; // 台北101
+  const selectedMode = "DRIVING";
+
+  directionsService
+    .route({
+      origin: start,
+      destination: end,
+      travelMode: google.maps.TravelMode[selectedMode],
+    })
+    .then((response) => {
+      directionsRenderer.setDirections(response);
+    })
+    .catch((e) => window.alert("Directions request failed"));
+}
+
+addNewPlan();
